69e3c13cbf0bd191c4afcb5d37e3d0c21ef815ba
Aditya Mukherjee <aditya@adityamukherjee.com>
2017-12-29 21:51:33 +0530

    improve completion declarations

--- completions/fasd.fish
+++ completions/fasd.fish
@@ -1,15 +1,31 @@
-complete -c fasd -f
-complete -c fasd -s s -d "List all paths and scores" -f
-complete -c fasd -s l -d "List paths without scores" -f
-complete -c fasd -s i -d "Interactive mode" -f
-complete -c fasd -s e -a "subl atom" -d "Set command to execute on the result file" -f
-complete -c fasd -s b -a "spotlight viminfo recently-used current" -d "Only use given backend" -f
-complete -c fasd -s B -a "spotlight viminfo recently-used current" -d "Add additional backend" -f
-complete -c fasd -s a -d "Match files and directories" -f
-complete -c fasd -s d -d "Match directories only" -f
-complete -c fasd -s f -d "Match files only" -f
-complete -c fasd -s r -d "Match by rank only" -f
-complete -c fasd -s t -d "Match by recent access only" -f
-complete -c fasd -s R -d "Reverse listing order" -f
-complete -c fasd -s h -d "Show a brief help message" -f
-# complete -c fasd -s [0-9] "Select the nth entry"
+function __print_fasd_completion_args -d "fasd internal / print possible values for given argument"
+  if test $argv = "e"; and echo "subl atom"; end
+  if test $argv = "b" -o $argv = "B"; and echo "spotlight viminfo recently-used current"; end
+  if test ! $argv = "e" -o ! $argv = "b" -o ! $argv = "B"; and echo ""; end
+end
+
+function __print_fasd_completion -d "fasd internal / print description for given argument"
+  if test $argv = "s"; and echo "List all paths and scores"; end
+  if test $argv = "l"; and echo "List paths without scores"; end
+  if test $argv = "i"; and echo "Interactive mode"; end
+  if test $argv = "e"; and echo "Set command to execute on the result file"; end
+  if test $argv = "b"; and echo "Only use given backend"; end
+  if test $argv = "B"; and echo "Add additional backend"; end
+  if test $argv = "a"; and echo "Match files and directories"; end
+  if test $argv = "d"; and echo "Match directories only"; end
+  if test $argv = "f"; and echo "Match files only"; end
+  if test $argv = "r"; and echo "Match by rank only"; end
+  if test $argv = "t"; and echo "Match by recent access only"; end
+  if test $argv = "R"; and echo "Reverse listing order"; end
+  if test $argv = "h"; and echo "Show a brief help message"; end
+end
+
+function __set_fasd_completion -d "fasd internal / set completion for all alphabetical arguments"
+  set -lx args s l i e b B a d f r t r h
+  for arg in $args
+    complete -c fasd -s "$arg" -d "(__print_fasd_completion $arg)" -a "(__print_fasd_completion_args $arg)" -f
+  end
+end
+
+complete -c fasd -f     # don't suggest files
+__set_fasd_completion   # set all completions
